fc84b880b22af9bb77b914c6595d9bd88c523f2f
yuanshaohang <yuanshaohang@bailian.ai>
2023-08-14 19:05:47 +0800

    add Basic information description

--- Production_spider.py
+++ Production_spider.py
@@ -5,4 +5,10 @@
 # @Description: 创建爬虫
 
 from config.spider_model import production
+# my_first为要创建的爬虫名称
+# 是否要增量运行（True  or  False）
+# 每次增量执行的页码
+# 所属人名称
+# 备注信息
+# 要创建的爬虫路径（路径不存在时自动创建）
 production('my_first', True, 5, '用户', '第一个爬虫', 'your_spider/my_first/')
